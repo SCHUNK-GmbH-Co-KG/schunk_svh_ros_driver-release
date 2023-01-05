5a567746f333cae9ce6910fd069f454e1474c999
Stefan Scherzinger <scherzin@fzi.de>
2023-01-05 18:33:44 +0100

    Revert "Trimmed the branch to only the schunk_svh sub directory"
    This reverts commit 0d25085ba447e3405f4d68585284fe5001bae2a5.

---
+++ schunk_svh_driver/include/SVHDiagnostics.h
@@ -0,0 +1,221 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// © Copyright 2022 SCHUNK Mobile Greifsysteme GmbH, Lauffen/Neckar Germany
+// © Copyright 2022 FZI Forschungszentrum Informatik, Karlsruhe, Germany
+//
+// This file is part of the Schunk SVH Driver.
+//
+// The Schunk SVH Driver is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The Schunk SVH Driver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the Schunk SVH Driver. If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Lars Pfotzer
+ * \author  Johannes Mangler
+ * \date    2015-10-23
+ *
+ */
+//----------------------------------------------------------------------
+
+#ifndef S5FH_DIAGNOSTICS_H
+#define S5FH_DIAGNOSTICS_H
+
+// ROS includes.
+#include <actionlib/server/simple_action_server.h>
+#include <ros/ros.h>
+
+#include <functional>
+#include <memory>
+#include <stdint.h>
+#include <string>
+
+// Date
+#include <ctime>
+#include <fstream>
+#include <iostream>
+
+// Messages
+#include "std_msgs/Float64MultiArray.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/MultiArrayLayout.h"
+#include <sensor_msgs/JointState.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/String.h>
+
+// FZI includes
+#include "schunk_svh_msgs/SVHDiagnosticsAction.h"
+#include "schunk_svh_msgs/SVHDiagnosticsToProtocol.h"
+
+// Driver Specific things
+#include <schunk_svh_library/SVHFirmwareInfo.h>
+#include <schunk_svh_library/control/SVHCurrentSettings.h>
+#include <schunk_svh_library/control/SVHFingerManager.h>
+#include <schunk_svh_library/control/SVHPositionSettings.h>
+
+class SVHDiagnostics
+{
+public:
+  //!
+  //! \brief SVHDiagnostics constructs a new node object that handles most of the functionality
+  //! \param nh ROS Nodehandle
+  //! \param finger_manager Handle to the SVH finger manager for library access
+  //! \param enable_ros_contol_loop function handle to set the ros-control-loop enabling flag
+  //! \param init_controller_parameters function handle to get the hand parameters
+  //! \param name action server name
+  //!
+  SVHDiagnostics(const ros::NodeHandle& nh,
+                 std::shared_ptr<driver_svh::SVHFingerManager>& finger_manager,
+                 std::function<void(bool)> enable_ros_contol_loop,
+                 std::function<void(uint16_t, uint16_t)> init_controller_parameters,
+                 std::string name);
+
+  //! Default DTOR
+  ~SVHDiagnostics();
+
+  //! Callback function to conduct the basic test for the SCHUNK five finger hand (msg defined in
+  //! schunk_web_gui/msg/DiagnosticsMsg.msg)
+  void basicTestCallback(const schunk_svh_msgs::SVHDiagnosticsGoalConstPtr& goal);
+
+  //! Callback function to test the subfunctions of SVHDiagnostics
+  void testCallback(const std_msgs::String&);
+
+
+private:
+  //!
+  //! \brief private node handle
+  //!
+  ros::NodeHandle m_priv_nh;
+
+  //!
+  //! \brief resetDiagnosticStatus resets the finger vector with the diagnostic data
+  //!
+  void resetDiagnosticStatus();
+
+  //!
+  //! \brief debugOutput prints the diagnostic data of all finger
+  //!
+  void debugOuput();
+
+  //!
+  //! \brief evaluateBasicTest evaluates the diagnostics status of the basic test to send the hint
+  //! informations to the webside \return std_msgs::UInt8MultiArray mesg with the test results
+  //!
+  schunk_svh_msgs::SVHDiagnosticsResult evaluateBasicTest();
+
+  //!
+  //! \brief set the protocol variables
+  //!
+  void qualityProtocolWritting();
+
+  //!
+  //! \brief initialize the protocol variables
+  //!
+  void initializeProtocolMessage();
+
+  //!
+  //! \brief initialize the protocol variables
+  //!
+  void initTest();
+
+  //! Handle to the SVH finger manager for library access
+  std::shared_ptr<driver_svh::SVHFingerManager> m_finger_manager;
+
+  //!
+  //! function handle to set the ros-control-loop enabling flag in the SVHWrapper
+  //!
+  std::function<void(bool)> m_enable_ros_contol_loop;
+
+  //!
+  //! function handle to get the hand parameters from ros-param-server
+  //! and set them to the finger_manager to have cleared parameter set
+  //!
+  std::function<void(uint16_t, uint16_t)> m_init_controller_parameters;
+
+  //! Serial device to use for communication with hardware
+  std::string m_serial_device_name;
+
+  //! Number of times the connect routine tries to connect in case that we receive at least one
+  //! package
+  int m_connect_retry_count;
+
+  //! Prefix for the driver to identify joint names if the Driver should expext "left_hand_Pinky"
+  //! than the prefix is left_hand
+  std::string name_prefix;
+
+  //! joint state message template
+  sensor_msgs::JointState channel_pos_;
+
+  //! Current Value message template
+  std_msgs::Float64MultiArray channel_currents;
+
+  //! Reset successed vector
+  std::vector<bool> reset_success;
+
+  //! Serial No of the tested Hand
+  std::string m_serial_no;
+
+  //! Name of the testing person
+  std::string m_assembly_of;
+
+  //! Type of the Hand, No/ L / R
+  bool m_execution_R;
+  bool m_execution_L;
+
+  //! Type of onnection
+  bool m_communication;
+
+  //! To catch multiple bastic test starts
+  bool m_basic_test_running;
+
+  //! publisher to the protocol variables
+  ros::Publisher m_pub_protocol_variables;
+
+  //! Message for the Protocol variable
+  schunk_svh_msgs::SVHDiagnosticsToProtocol m_msg_protocol_variable;
+
+  //! Action Server
+  actionlib::SimpleActionServer<schunk_svh_msgs::SVHDiagnosticsAction> m_diagnostics_action_server;
+
+  // Action Feedback
+  schunk_svh_msgs::SVHDiagnosticsFeedback m_action_feedback;
+
+  //! Action result
+  schunk_svh_msgs::SVHDiagnosticsResult m_action_result;
+
+  // Action server name
+  std::string m_action_name;
+
+  enum diag_information
+  {
+    zero_defect,
+    board_one,
+    board_two,
+    board_one_encoder,
+    board_one_motor,
+    board_two_encoder,
+    board_two_motor,
+    encoder_motor,
+    encoder,
+    motor,
+    current_range,
+    position_range,
+    diag_information_dimension
+  };
+};
+
+#endif // S5FH_CONTROLLER_H
---
+++ schunk_svh_driver/include/SVHRosControlHWInterface.h
@@ -0,0 +1,112 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// © Copyright 2022 SCHUNK Mobile Greifsysteme GmbH, Lauffen/Neckar Germany
+// © Copyright 2022 FZI Forschungszentrum Informatik, Karlsruhe, Germany
+//
+// This file is part of the Schunk SVH Driver.
+//
+// The Schunk SVH Driver is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The Schunk SVH Driver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the Schunk SVH Driver. If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Andreas Hermann <hermann@fzi.de>
+ * \date    2016-02-19
+ *
+ */
+//----------------------------------------------------------------------
+
+#ifndef SCHUNK_SVH_HARDWARE_INTERFACE_H_
+#define SCHUNK_SVH_HARDWARE_INTERFACE_H_
+
+// Driver Specific things
+#include "SVHWrapper.h"
+
+#include <controller_manager/controller_manager.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <joint_limits_interface/joint_limits.h>
+#include <memory>
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+
+/*!
+ * \brief This class defines a ros-control hardware interface.
+ *
+ */
+class SVHRosControlHWInterface : public hardware_interface::RobotHW
+{
+public:
+  SVHRosControlHWInterface();
+  ~SVHRosControlHWInterface();
+
+  /// \brief Initialize the hardware interface
+  virtual bool init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_hw_nh);
+  //   virtual void init();
+
+  /// \brief Read the state from the robot hardware.
+  virtual void read(const ros::Time& time, const ros::Duration& period);
+
+  /// \brief write the command to the robot hardware.
+  virtual void write(const ros::Time& time, const ros::Duration& period);
+
+  bool prepareSwitch(const std::list<hardware_interface::ControllerInfo>& start_list,
+                     const std::list<hardware_interface::ControllerInfo>& stop_list);
+  void doSwitch(const std::list<hardware_interface::ControllerInfo>& start_list,
+                const std::list<hardware_interface::ControllerInfo>& stop_list);
+
+
+  bool isEnabled() const;
+  /*!
+   * \brief Returns true, when at least one node in the hardware is in a fault state.
+   */
+  bool isFault() { return m_is_fault; }
+  /*!
+   * \brief Creates a joint_state message from the current joint angles and returns it.
+   */
+  //   sensor_msgs::JointState getJointMessage();
+
+protected:
+  ros::NodeHandle m_node_handle;
+  //! Handle to the SVH finger manager for library access
+  std::shared_ptr<SVHWrapper> m_svh;
+
+  // Interfaces
+  hardware_interface::JointStateInterface m_joint_state_interface;
+  hardware_interface::PositionJointInterface m_position_joint_interface;
+
+  size_t m_num_joints;
+
+  std::vector<uint8_t> m_node_ids;
+  std::vector<std::string> m_channel_names; // Combines prefix with channel_description
+  std::vector<double> m_joint_positions;
+  std::vector<double> m_joint_velocity;
+  std::vector<double> m_joint_effort;
+  std::vector<double> m_joint_position_commands;
+
+  bool m_is_fault;
+
+  joint_limits_interface::JointLimits m_joint_limits;
+  joint_limits_interface::SoftJointLimits
+    m_joint_soft_limits; // only available through URDF, currently not used.
+
+private:
+  bool m_hardware_ready;
+};
+
+
+#endif /*SCHUNK_SVH_HARDWARE_INTERFACE_H_*/
